c7cf012ba91804688a0767a64fb3e75a26b18840
zearkiatos <caprilespe@outlook.com>
2023-02-20 20:37:09 -0300

    Application logic

---
+++ src/python_my_grade_app/logic/subject_organizer.py
@@ -0,0 +1,23 @@
+class subject_organizer():
+    def __init__(self):
+        self.subjects_list = []
+        self.subjects_list.append({"name": "Introducción a la programación",
+                                  "semestry": "2019-1", "professor": "Antonio Andrade", "grade": 4.5})
+        self.subjects_list.append({"name": "Estructuras de datos",
+                                  "semestry": "2019-2", "professor": "Hamid Abdallah", "grade": 3.5})
+        self.subjects_list.append({"name": "Desarrollo de software",
+                                  "semestry": "2020-1", "professor": "Rubby Casallas", "grade": 2.0})
+        self.subjects_list.append({"name": "Arquitectura de software",
+                                  "semestry": "2020-2", "professor": "Xiao Lihua", "grade": 4.0})
+        self.current = 0
+
+    def get_current_subject(self):
+        return self.subjects_list[self.current]
+
+    def advance(self):
+        self.current += 1
+        self.current = self.current % len(self.subjects_list)
+
+    def back(self):
+        self.current -= 1
+        self.current self.current % len(self.subjects_list)

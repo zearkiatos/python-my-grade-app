771e910f851644807558f9dc97dbe48fa7beb172
zearkiatos <caprilespe@outlook.com>
2023-02-20 22:33:17 -0300

    Distribution graphs

--- src/python_my_grade_app/gui/subject_viewer.py
+++ src/python_my_grade_app/gui/subject_viewer.py
@@ -15,4 +15,19 @@ class Gui_Application(QWidget):
     def initialize_gui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.vertical_distribution = QVBoxLayout()
+        self.box_subject = QGroupBox("Subject")
+        distribution_box_subject = QGridLayout()
+        self.box_subject.setLayout(distribution_box_subject)
+
+        self.box_buttons = QGroupBox()
+        distribution_box_buttons = QHBoxLayout()
+        self.box_buttons.setLayout(distribution_box_buttons)
+
+        self.vertical_distribution.addWidget(self.box_subject)
+        self.vertical_distribution.addWidget(self.box_buttons)
+
+        self.setLayout(self.vertical_distribution)
+
         self.show()
